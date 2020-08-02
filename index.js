808f894caafe744ffaf09c72cf8429beba36a8d4
Sondos Ahmed <sondosalshami@gmail.com>
2020-08-02 23:17:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+class Cat {
+  //...
+  constructors(name){
+    this.name=name;
+  }
+  speak(){
+    return name+" says meow"
+  }
+}
+
+class Dog {
+  //...
+}
+
+class Bird {
+  //...
+}
